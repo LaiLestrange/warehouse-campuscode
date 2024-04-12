e4f8abf05aa8435fa982d0f3516f7003c653e53d
Lais Almeida <laisdev25@gmail.com>
2024-04-12 16:49:13 -0300

    Removendo repetições de código

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,17 +1,15 @@
 class WarehousesController < ApplicationController
-  def show
-    id = params[:id]
-    @warehouse = Warehouse.find(id)
-  end
+  before_action :set_warehouse, only: [:show, :edit, :update]
+
+  def show ; end
 
   def new
-    @new_warehouse = Warehouse.new
+    @warehouse = Warehouse.new
   end
 
   def create
-    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :area, :address, :cep, :description)
-    @new_warehouse = Warehouse.new(warehouse_params)
-    if @new_warehouse.save()
+    @warehouse = Warehouse.new(warehouse_params)
+    if @warehouse.save()
       redirect_to root_path, notice: "Galpão cadastrado com sucesso!"
     else
       flash.now[:notice] = "Galpão não cadastrado!"
@@ -19,20 +17,24 @@ class WarehousesController < ApplicationController
     end
   end
 
-  def edit
-    id = params[:id]
-    @warehouse = Warehouse.find(id)
-  end
+  def edit ; end
 
   def update
-    id = params[:id]
-    @warehouse = Warehouse.find(id)
-    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :area, :address, :cep, :description)
     if @warehouse.update(warehouse_params)
-      redirect_to warehouse_path(id), notice: "Galpão atualizado com sucesso!"
+      redirect_to warehouse_path(@warehouse.id), notice: "Galpão atualizado com sucesso!"
     else
       flash.now[:notice] = "Não foi possível atualizar o galpão!"
       render 'edit'
     end
   end
+
+  private
+  def set_warehouse
+    @warehouse = Warehouse.find(params[:id])
+  end
+
+  def warehouse_params
+    params.require(:warehouse).permit(:name, :code, :city, :area, :address, :cep, :description)
+  end
+
 end
