f0bfe9df980ce3515d48f672a51101c3236b587d
Lais Almeida <laisdev25@gmail.com>
2024-04-14 13:27:15 -0300

    Removendo um galpão

---
+++ spec/system/user_delete_warehouse_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe 'Usuário remove um galpão' do
+  it 'com sucesso' do
+    #Arrange
+    Warehouse.create!(name: 'Aero SSA', code:  'EVA', city: 'Salvador', area:210_000,
+    address: 'Estrada Velha do Aeroporto, 5', cep: '17000-000',
+    description: 'Galpão da última astronave')
+    
+    #Act
+    visit root_path
+    click_on 'Aero SSA'
+    click_on 'Remover'
+
+    #Assert
+    expect(current_path).to eq root_path
+    expect(page).to have_content("Galpão removido com sucesso")
+    expect(page).not_to have_content("Aero SSA")
+    expect(page).not_to have_content("Código: EVA")
+
+  end
+
+  it 'e não apaga outros galpões' do
+    #Arrange
+    first_warehouse = Warehouse.create!(name: 'Aero SSA', code:  'EVA', city: 'Salvador', area:210_000,
+    address: 'Estrada Velha do Aeroporto, 5', cep: '17000-000',
+    description: 'Galpão da última astronave')
+    second_warehouse = Warehouse.create!(name: 'Aero VCA', code:  'VDC', city: 'Vitória da Conquista', area:217_000,
+    address: 'Estrada da Suiça Baiana, 77', cep: '77000-000',
+    description: 'Galpão da boca do inferno')
+
+
+    #Act
+    visit root_path
+    click_on 'Aero VCA'
+    click_on 'Remover'
+
+    #Assert
+    expect(current_path).to eq root_path
+    expect(page).to have_content "Galpão removido com sucesso"
+    expect(page).not_to have_content "Aero VCA"
+    expect(page).not_to have_content "Código: VDC"
+    expect(page).to have_content "Aero SSA"
+    expect(page).to have_content "Código: EVA"
+
+  end
+end
