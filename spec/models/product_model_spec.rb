010d227dd3f0e0b55f34c72615646bfe1d18fa92
Lais Almeida <laisdev25@gmail.com>
2024-04-17 03:23:32 -0300

    Adicionando Product Models

--- spec/models/product_model_spec.rb
+++ spec/models/product_model_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe ProductModel, type: :model do
- # pending "add some examples to (or delete) #{__FILE__}"
+  describe "#valid?" do
+    context "presence" do
+      it "name is mandatory" do
+        supplier = Supplier.create!(
+          corporate_name: 'Nome Empresa 1 Oficial LTDA',
+          brand_name: 'Empresa 1',
+          registration_number: '10',
+          full_address: 'Rua das Empresas, 1',
+          city: 'Empresarial',
+          state: 'EM',
+          email: 'empresa1@empresas.com'
+        )
+        pm = ProductModel.new(
+          name: '',
+          weight: 10,
+          width: 11,
+          height: 12,
+          depth: 13,
+          sku: '101010',
+          supplier: supplier
+        )
+        expect(pm.valid?).to eq false
+
+      end
+      it "sku is mandatory" do
+        supplier = Supplier.create!(
+          corporate_name: 'Nome Empresa 1 Oficial LTDA',
+          brand_name: 'Empresa 1',
+          registration_number: '10',
+          full_address: 'Rua das Empresas, 1',
+          city: 'Empresarial',
+          state: 'EM',
+          email: 'empresa1@empresas.com'
+        )
+        pm = ProductModel.new(
+          name: 'Produto A',
+          weight: 10,
+          width: 11,
+          height: 12,
+          depth: 13,
+          sku: '',
+          supplier: supplier
+        )
+        expect(pm.valid?).to eq false
+
+      end
+    end
+  end
+
 end
---
+++ spec/system/product_models/user_register_product_models_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+
+describe "Usuário cadastra um modelo de produto" do
+  it "com sucesso" do
+    #Arrange
+    first_supplier = Supplier.create!(
+      corporate_name: 'Nome Empresa 1 Oficial LTDA',
+      brand_name: 'Empresa 1',
+      registration_number: '10',
+      full_address: 'Rua das Empresas, 1',
+      city: 'Empresarial',
+      state: 'EM',
+      email: 'empresa1@empresas.com'
+    )
+
+    second_supplier = Supplier.create!(
+      corporate_name: 'Nome Empresa 2 Oficial LTDA',
+      brand_name: 'Empresa 2',
+      registration_number: '20',
+      full_address: 'Rua das Empresas, 2',
+      city: 'Empresarial',
+      state: 'EM',
+      email: 'empresa2@empresas.com'
+    )
+    #Act
+    visit root_path
+    within('nav') do
+      click_on 'Modelos de Produtos'
+    end
+    click_on 'Cadastrar Novo'
+    fill_in 'Nome', with: 'Produto A'
+    fill_in 'Peso', with: '10'
+    fill_in 'Largura', with: '11'
+    fill_in 'Altura', with: '12'
+    fill_in 'Profundidade', with: '13'
+    fill_in 'SKU', with: 'A10111213'
+    select first_supplier.brand_name, from: 'Fornecedor' #escolher um
+    select second_supplier.brand_name, from: 'Fornecedor' #e comentar o outro
+    click_on 'Criar Modelo de Produto'
+    #Assert
+    expect(page).to have_content 'Modelo de Produto cadastrado com sucesso!'
+    expect(page).to have_content 'Produto A'
+    expect(page).to have_content 'A10111213'
+    expect(page).to have_content 'Peso: 10g'
+    expect(page).to have_content 'Dimensão: 11cm x 12cm x 13cm'
+    expect(page).to have_content 'Fornecedor: Empresa 2' #ajustar qual recebe
+  end
+
+  it 'deve preencher todos os campos' do
+    #Arrange
+    Supplier.create!(
+      corporate_name: 'Nome Empresa 1 Oficial LTDA',
+      brand_name: 'Empresa 1',
+      registration_number: '10',
+      full_address: 'Rua das Empresas, 1',
+      city: 'Empresarial',
+      state: 'EM',
+      email: 'empresa1@empresas.com'
+    )
+
+    #Act
+    visit root_path
+    within('nav') do
+      click_on 'Modelos de Produtos'
+    end
+    click_on 'Cadastrar Novo'
+    fill_in 'Nome', with: ''
+    fill_in 'Peso', with: ''
+    click_on 'Criar Modelo de Produto'
+
+    #Assert
+    expect(page).to have_content 'Não foi possível cadastrar o modelo de produto!'
+  end
+
+end
