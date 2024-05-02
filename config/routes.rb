51995a22440ab6bb0d74112e78335603d083caee
Lais Almeida <laisdev25@gmail.com>
2024-05-01 22:16:53 -0300

    Usuário vê seus pedidos

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :warehouses, only: [:show, :new, :create, :edit , :update, :destroy]
   resources :suppliers, only: [:index, :show, :new, :create, :edit, :update]
-  resources :orders, only: [:show, :new, :create] do
+  resources :orders, only: [:new, :create, :show, :index] do
     get 'search', on: :collection
   end
 
---
+++ spec/system/orders/user_view_own_orders_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+describe "Usuário vê seus próprios pedidos" do
+  it "e deve estar autenticado" do
+    #Arrange
+
+    #Act
+    visit root_path
+    click_on 'Meus Pedidos'
+
+    #Assert
+    expect(current_path).to eq new_user_session_path
+
+  end
+  it "e não vê outros pedidos" do
+    #Arrange
+    first_user = User.create!(name: "Cliente1", email: "email1@cliente.com", password: 'cl13n73')
+    second_user = User.create!(name: "Cliente2", email: "email2@cliente.com", password: 'cl13n73')
+
+    supplier = Supplier.create!(corporate_name: 'Empresa X nome oficial registrado',
+                                brand_name: 'Fantasia EMPRESA X',
+                                registration_number: '123465789-00',
+                                full_address: 'Rua abc, 540',
+                                city: 'Cidade',
+                                state: 'UE',
+                                email: 'empresax@email.com')
+    warehouse = Warehouse.create!(name: 'Aeroporto SP',
+                                  code:  'GRU',
+                                  city: 'Guarulhos',
+                                  area:100_000,
+                                  address: 'Avenida do Aeroporto, 1000',
+                                  cep: '15000-000',
+                                  description: 'Galpão destinado para cargas internacionais')
+    first_order = Order.create!(user: first_user,
+                          warehouse: warehouse,
+                          supplier: supplier,
+                          estimated_delivery_date: 1.day.from_now)
+    second_order = Order.create!(user: first_user,
+                          warehouse: warehouse,
+                          supplier: supplier,
+                          estimated_delivery_date: 1.week.from_now)
+    third_order = Order.create!(user: second_user,
+                          warehouse: warehouse,
+                          supplier: supplier,
+                          estimated_delivery_date: 1.month.from_now)
+
+    #Act
+    login_as first_user
+    visit root_path
+    click_on 'Meus Pedidos'
+
+    #Assert
+    expect(page).to have_content first_order.code
+    expect(page).to have_content second_order.code
+    expect(page).not_to have_content third_order.code
+  end
+end
